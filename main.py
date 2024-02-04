9dce5273cfaa5ee250df6deae20852bf33dc16ee
Peshal <71926120+Peshal-git@users.noreply.github.com>
2024-02-04 17:04:04 +0545

    Initial

---
+++ main.py
@@ -0,0 +1,8 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.core.window import Window
+Window.size = (360,600)
+class MainApp(MDApp):
+    def build(self):
+        pass
+MainApp().run()
